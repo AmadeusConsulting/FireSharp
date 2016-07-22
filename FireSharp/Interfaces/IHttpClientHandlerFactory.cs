d5d9f9694fafbcff1fba45f83c95c5587db33c27
Kevin Jones <kjones@amadeusconsulting.com>
2016-07-22 16:01:36 -0600

    Add Missing Interface File

---
+++ FireSharp/Interfaces/IHttpClientHandlerFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace FireSharp.Interfaces
+{
+    public interface IHttpClientHandlerFactory
+    {
+        HttpMessageHandler CreateHandler(bool allowAutoRedirects = false);
+    }
+}
